4496bb6554c03e90abc87106a7555f5a3ecf0d7b
joaogabriels <joaogabrielsevangelista@gmail.com>
2022-05-07 11:51:58 -0300

    Working on auth system

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,19 +1,22 @@
-import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
+import styles from './styles.module.scss';
 
 export function SingInButton() {
-    const isUserLoggedIn = true;
+    const {data: session} = useSession()
     
+    console.log(session);
     
-    return isUserLoggedIn ? ( 
+    return session ? ( 
         <button 
         type="button"
         className={styles.singInButton}
+        onClick={() => signOut()}
         >
            <FaGithub color="#04d361"/>
-            João Gabriel
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ) : 
@@ -21,6 +24,7 @@ export function SingInButton() {
         <button 
         type="button"
         className={styles.singInButton}
+        onClick={() => signIn('github')}
         >
            <FaGithub color="#EBA417"/>
             Sing in with Github
